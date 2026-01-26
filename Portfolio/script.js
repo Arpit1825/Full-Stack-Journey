642474cd78f47847bba643484584fba323b2cb55
Arpit1825 <2k24.csaiml1a2413757@gmail.com>
2026-01-26 14:11:57 +0530

    Added Dark Mode Theme Toggle and Contact Form Alert features

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,5 +1,21 @@
-const btn=document.getElementById('butn');
-button.addEventListener('click', function() {
-    alert("Badhiya Arpit Bhai! Events kaam kar rahe hain. 🚀");
-    console.log("Button was clicked successfully.");
+const btn = document.getElementById('butn');
+
+btn.addEventListener('click', function() {
+    alert('Button Pressed!')
+    document.body.classList.toggle('dark-mode');
+     console.log("Button was clicked successfully and Theme is changing.");
+
+});
+const form=document.querySelector("form");
+form.addEventListener("submit", function(e) {
+    e.preventDefault();  
+
+    const fullnam=document.querySelector(".fullnm").value;
+    const email=document.getElementById("email").value;
+    const message = document.getElementById("msg").value;
+    if(fullnam.trim() !=="" && email.trim() !=="" && message.trim() !== ""){
+        alert("Form Submitted!");
+    }else{
+        alert("Not Submitted!");
+    }
 });
