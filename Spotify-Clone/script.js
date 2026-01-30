cf48325a5198e5b9322921b4ab07c06c80c29f65
Arpit1825 <2k24.csaiml1a2413757@gmail.com>
2026-01-31 03:18:57 +0530

    Added SVG icons and Sidebar Layout structure

---
+++ Spotify-Clone/script.js
@@ -0,0 +1,45 @@
+console.log("Welcome to Spotify");
+
+let songIndex = 0;
+let audioElement = new Audio('/Spotify-Clone/songs/1.mp3'); 
+let masterPlay = document.getElementById('masterPlay'); 
+let myProgressBar = document.getElementById('myProgressBar'); 
+let gif = document.getElementById('gif'); 
+let songs=[{songname:"FAVELA",filepath:"/Spotify-Clone/songs/1.mp3",coverpath:"/Spotify-Clone/covers/1.jpg"},{songname:"Fearless Funk",filepath:"/Spotify-Clone/songs/2.mp3",coverpath:"/Spotify-Clone/covers/2.jpg"},{songname:"Fly High",filepath:"/Spotify-Clone/songs/3.mp3",coverpath:"/Spotify-Clone/covers/3.jpg"},{songname:"ALL IN MY HIGH",filepath:"/Spotify-Clone/songs/4.mp3",coverpath:"/Spotify-Clone/covers/4.jpg"},{songname:"Where We Are",filepath:"/Spotify-Clone/songs/5.mp3",coverpath:"/Spotify-Clone/covers/5.jpg"},{songname:"LOFIN-scars",filepath:"/Spotify-Clone/songs/6.mp3",coverpath:"/Spotify-Clone/covers/6.jpg"},{songname:"Alone Tonight",filepath:"/Spotify-Clone/songs/7.mp3",coverpath:"/Spotify-Clone/covers/7.jpg"},{songname:"Godless-Ailow",filepath:"/Spotify-Clone/songs/8.mp3",coverpath:"/Spotify-Clone/covers/8.jpg"},{songname:"Hero's Ending",filepath:"/Spotify-Clone/songs/9.mp3",coverpath:"/Spotify-Clone/covers/9.jpg"},{songname:"Warrior-Marin Hox",filepath:"/Spotify-Clone/songs/10.mp3",coverpath:"/Spotify-Clone/covers/10.jpg"}]
+console.log(songs);
+let songItems=Array.from(document.getElementsByClassName('songItem'));
+
+
+const sidebar = document.getElementById('sidebar'); // HTML mein id="sidebar" hona chahiye
+const resizer = document.getElementById('dragHandle'); // HTML mein id="dragHandle" hona chahiye
+
+let isResizing = false;
+
+// Mouse dabaya
+resizer.addEventListener('mousedown', (e) => {
+    isResizing = true;
+    
+    // 🔥 Fix: Text selection rokne ke liye
+    e.preventDefault(); 
+    
+    document.body.style.cursor = 'col-resize';
+    console.log("Resizing shuru!"); // Console mein check karna ye print hua ya nahi
+});
+
+// Mouse hilaya
+document.addEventListener('mousemove', (e) => {
+    if (!isResizing) return;
+
+    // 🔥 Fix: Selection glitch rokna
+    e.preventDefault();
+
+    // Width update karo
+    sidebar.style.width = e.clientX + 'px';
+});
+
+// Mouse chhoda
+document.addEventListener('mouseup', () => {
+    isResizing = false;
+    document.body.style.cursor = 'default';
+    console.log("Resizing khatam.");
+});
