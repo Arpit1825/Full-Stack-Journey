09b131fb285a7036b6dc01a4cafccf03db28b737
Arpit1825 <2k24.csaiml1a2413757@gmail.com>
2026-02-05 13:53:31 +0530

    Added pause play button logic

--- Spotify-Clone/script.js
+++ Spotify-Clone/script.js
@@ -45,3 +45,16 @@ document.addEventListener('mouseup', () => {
     document.body.style.cursor = 'default';
     console.log("Resizing khatam.");
 });
+const playPath="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14H9V8h2v8zm4 0h-2V8h2v8z";
+const pausePath="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z";
+let masterplay=document.querySelector("#masterPlay");
+masterplay.addEventListener("click",(det)=>{
+     currentPath=det.target.getAttribute("d");
+     if(currentPath==pausePath){
+        det.target.setAttribute("d",playPath);
+     }else{
+        det.target.setAttribute("d",pausePath);
+     }
+
+});
+
